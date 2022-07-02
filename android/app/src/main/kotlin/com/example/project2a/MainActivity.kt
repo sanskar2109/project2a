d2a665ac5a875eb0efc66af4ac325a96ea113d5b
Sanskar Singhal <89839415+sanskar2109@users.noreply.github.com>
2022-07-02 15:45:11 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/project2a/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project2a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Login.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:project2a/responsive.dart';
+import 'loginform.dart';
+
+import '../../components/background.dart';
+import 'loginscreen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Background(
+      child: SingleChildScrollView(
+        child: Responsive(
+          mobile: const MobileLoginScreen(),
+          desktop: Row(
+            children: [
+              const Expanded(
+                child: LoginScreen(),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    SizedBox(
+                      width: 450,
+                      child: loginform(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileLoginScreen extends StatelessWidget {
+  const MobileLoginScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const LoginScreen(),
+        Row(
+          children: const [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: LoginForm(),
+            ),
+            Spacer(),
+          ],
+        ),
+      ],
+    );
+  }
+}
